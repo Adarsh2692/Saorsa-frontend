0980324281f46c7c9d0855d60c5a7abd97bd1172
Adarsh2692 <adarsh.singh2692@gmail.com>
2021-11-03 13:08:14 +0530

    thumbneils changed

--- src/Components/HomePage/SixStep.js
+++ src/Components/HomePage/SixStep.js
@@ -5,7 +5,7 @@ import './Step.css';
 const SixStep = () => {
 	return (
 		<div class='stepcard'>
-			<div class='free1'>
+			{/* <div class='free1'>
 				<StepCard
 					img="https://res.cloudinary.com/adarshsingh/image/upload/v1627509657/All%20Course%20BG/STEPS-01_ggejtm.jpg"
 					to='s0'
@@ -45,6 +45,48 @@ const SixStep = () => {
 					img="https://res.cloudinary.com/adarshsingh/image/upload/v1627509751/All%20Course%20BG/STEPS-06_gqmbtr.jpg"
 					to='profile'
 					text="Your Personal Plan"
+				/>
+			</div> */}
+			<div class='free1'>
+				<StepCard
+					img='https://res.cloudinary.com/adarshsingh/image/upload/v1635924862/Step_1_Emotional_Awareness_Thumbnail_i5e0jn.png'
+					to='s0'
+					text='Emotional Awareness'
+				/>
+			</div>
+			<div class='free2'>
+				<StepCard
+					img='https://res.cloudinary.com/adarshsingh/image/upload/v1635924862/Step_2_Understanding_Worry_anxiety_and_Uncertainty_Thumbnail_gkib5o.png'
+					to='s1'
+					text='Uncertainty: Worry And Anxiety'
+				/>
+			</div>
+			<div class='free1'>
+				<StepCard
+					img='https://res.cloudinary.com/adarshsingh/image/upload/v1635924862/Step_3_Learning_to_self_soothe_thumbnail_zgpp3l.png'
+					to='s2'
+					text='LEARNING TO SELF SOOTHE'
+				/>
+			</div>
+			<div class='free2'>
+				<StepCard
+					img='https://res.cloudinary.com/adarshsingh/image/upload/v1635924861/Step_4_unhelpful_coping_thumbnail_j0gpii.png'
+					to='s3'
+					text='CHANGING UNHEALTHY COPING BEHAVIOURS'
+				/>
+			</div>
+			<div class='free1'>
+				<StepCard
+					img='https://res.cloudinary.com/adarshsingh/image/upload/v1635924861/Step_5_mental_wellbeing_thumbnail_rxrdop.png'
+					to='s4'
+					text='SLEEP BETTER'
+				/>
+			</div>
+			<div class='free2'>
+				<StepCard
+					img='https://res.cloudinary.com/adarshsingh/image/upload/v1627509751/All%20Course%20BG/STEPS-06_gqmbtr.jpg'
+					to='profile'
+					text='Your Personal Plan'
 				/>
 			</div>
 		</div>
